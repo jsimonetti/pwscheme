6f4cbd4de500538963882d200c8492699d2ce281
Jeroen Simonetti <jeroen@simonetti.nl>
2016-02-09 18:33:33 +0100

    Add additional test cases
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- ssha256/ssha256_test.go
+++ ssha256/ssha256_test.go
@@ -6,27 +6,52 @@ import (
 	"github.com/jsimonetti/pwscheme/ssha256"
 )
 
-func TestValidate(t *testing.T) {
+func TestValidPassword(t *testing.T) {
 	pass := "test123"
 	hash := "{SSHA256}czO44OTV17PcF1cRxWrLZLy9xHd7CWyVYplr1rOhuMlx/7IK"
 
-	if res, err := ssha256.Validate(pass, hash); res != true {
-		t.Error("Validate password fails", err)
+	if res, err := ssha256.Validate(pass, hash); err != nil || res != true {
+		t.Errorf("Valid password fails validation: %s", err)
 	}
 }
 
-func TestGenerate(t *testing.T) {
+func TestInValidPassword(t *testing.T) {
+	pass := "test12"
+	hash := "{SSHA256}czO44OTV17PcF1cRxWrLZLy9xHd7CWyVYplr1rOhuMlx/7IK"
+
+	if res, err := ssha256.Validate(pass, hash); res != false {
+		t.Errorf("Invalid password passes validation: %s", err)
+	}
+}
+
+func TestGenerate4(t *testing.T) {
+	pass := "test123"
+	var hash string
+	var err error
+	var res bool
+
+	if hash, err = ssha256.Generate(pass, 4); err != nil {
+		t.Errorf("Generate password fails: %s", err)
+		return
+	}
+
+	if res, err = ssha256.Validate(pass, hash); err != nil && res != false {
+		t.Errorf("Generated hash can not be validated: %s", err)
+	}
+}
+
+func TestGenerate8(t *testing.T) {
 	pass := "test123"
 	var hash string
 	var err error
 	var res bool
 
 	if hash, err = ssha256.Generate(pass, 8); err != nil {
-		t.Error("Generate password fails", err)
+		t.Errorf("Generate password fails: %s", err)
 		return
 	}
 
 	if res, err = ssha256.Validate(pass, hash); err != nil && res != false {
-		t.Error("Generated hash can not be validated", err)
+		t.Errorf("Generated hash can not be validated: %s", err)
 	}
 }
